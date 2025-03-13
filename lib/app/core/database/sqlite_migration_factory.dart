72d8cc8d751e6096faa771773c4194b0910e087d
CSIRamone <cidinei_inacio@hotmail.com>
2025-03-13 17:07:27 -0300

    Configurando as Migrations

---
+++ lib/app/core/database/sqlite_migration_factory.dart
@@ -0,0 +1,33 @@
+
+import 'package:todo_list_provider/app/core/database/migrations/migration.dart';
+import 'package:todo_list_provider/app/core/database/migrations/migration_v1.dart';
+import 'package:todo_list_provider/app/core/database/migrations/migration_v2.dart';
+import 'package:todo_list_provider/app/core/database/migrations/migration_v3.dart';
+
+class SqliteMigrationFactory {
+  List<Migration> getCreateMigration() => [
+    MigrationV1(),
+    MigrationV2(),
+    MigrationV3(),
+  ];
+
+  List<Migration> getUpgradeMigration(int version) {
+    final migrations = <Migration>[];
+
+    //atual = 3
+    // version dop usuario = 2
+
+    if(version == 1){
+      migrations.add(MigrationV2());
+      migrations.add(MigrationV2());
+    }
+
+    if(version == 2){
+      migrations.add(MigrationV3());
+    } 
+
+    return migrations;
+
+  }
+
+}
