4cd36d93639d7d75d8a37291461f939906c424bb
CSIRamone <cidinei_inacio@hotmail.com>
2025-04-14 14:54:08 -0300

    Criando utilitarios de controle de loader, success e error

---
+++ lib/app/core/notifier/todolist_default_listener_notifier.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
+import 'package:todo_list_provider/app/core/notifier/todolist_default_change_notifier.dart';
+
+import '../ui/messages.dart';
+
+class TodolistDefaultListenerNotifier {
+  final TodolistDefaultChangeNotifier changeNotifier;
+
+  TodolistDefaultListenerNotifier({
+    required this.changeNotifier,
+  });
+
+  void listener({
+    required BuildContext context,
+    required SuccessVoidCallback successCallback,
+    ErrorVoidCallback? errorCallback,
+  }) {
+    changeNotifier.addListener(() {
+      if (changeNotifier.loading) {
+        Loader.show(context);
+      } else {
+        Loader.hide();
+      }
+
+      if (changeNotifier.hasError) {
+        if(errorCallback != null) {
+          errorCallback(changeNotifier, this);
+        }
+        // Show error message
+        Messages.of(context).showError(changeNotifier.error ?? 'Erro interno');
+      }
+
+      if (changeNotifier.isSuccess) {
+        successCallback(changeNotifier, this);
+      }
+    });
+  }
+
+  void dispose() {
+    changeNotifier.removeListener(() {});
+  }
+}
+
+typedef SuccessVoidCallback = void Function(
+    TodolistDefaultChangeNotifier notifier,
+    TodolistDefaultListenerNotifier listenerIntance);
+
+typedef ErrorVoidCallback = void Function(
+    TodolistDefaultChangeNotifier notifier,
+    TodolistDefaultListenerNotifier listenerIntance);
+
--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/notifier/todolist_default_change_notifier.dart';
 import 'package:todo_list_provider/app/repositories/exception/auth_exception.dart';
 import 'package:todo_list_provider/app/services/user/user_service.dart';
 
-class RegisterController extends ChangeNotifier {
+class RegisterController extends TodolistDefaultChangeNotifier {
   final UserService _userService;
-  String? error;
-  bool success = false;
+ 
 
   RegisterController({required UserService userService})
       : _userService = userService;
 
   Future<void> registerUser(String email, String password) async {
     try {
-      error = null;
-      success = false;
+      showLoadingAndResetState();
       notifyListeners();
       final user = await _userService.register(email, password);
       if (user != null) {
         // Handle successful registration (e.g., navigate to another screen)
-        success = true;
+        success();
       } else {
         // Handle registration error (e.g., show error message)
-        error = 'Registration failed';
+       setError('Registration failed');
       }
       notifyListeners();
     } on AuthException catch (e) {
-      error = e.message;
+      setError(e.message);
     } finally {
+      hideLoading();
       notifyListeners();
     }
   }
